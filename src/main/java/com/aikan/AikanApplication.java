aa8c9533fdbf56f1f4c360f574ebb38dcb714413
Ivanio Martins <ivanio.oliveira@mv.com.br>
2024-01-10 13:06:02 -0300

    Primeiro Commit

---
+++ src/main/java/com/aikan/AikanApplication.java
@@ -0,0 +1,13 @@
+package com.aikan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aikan/AikanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aikan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
