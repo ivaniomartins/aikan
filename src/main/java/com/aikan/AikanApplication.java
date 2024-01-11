f1c413a5d31589f8dc7fe9c608f85ecbecf88eb8
Ivanio Martins <ivanio.oliveira@mv.com.br>
2024-01-11 13:17:38 -0300

    Criação da interface PacienteRepository

--- src/main/java/com/aikan/AikanApplication.java
+++ src/main/java/com/aikan/AikanApplication.java
@@ -1,5 +1,6 @@
 package com.aikan;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aikan.model.Paciente;
 import com.aikan.model.Tutor;
+import com.aikan.model.enuns.Especie;
+import com.aikan.repositories.PacienteRepository;
 import com.aikan.repositories.TutorRepository;
 
 @SpringBootApplication
@@ -15,6 +19,9 @@ public class AikanApplication implements CommandLineRunner{
 	
 	@Autowired
 	private TutorRepository tutorRepository;
+	
+	@Autowired
+	private PacienteRepository pacienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AikanApplication.class, args);
@@ -22,9 +29,12 @@ public class AikanApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Tutor t1 = new Tutor(null, "Ivanio Martins", "092.348.134-64", "ivaniomartins1991@gmail.com", null);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
+		Tutor t1 = new Tutor(null, "Ivanio Martins", "092.348.134-64", "ivaniomartins1991@gmail.com", null);
+		Paciente p1 = new Paciente(null, "Nakia", "American-Bully",20.1,sdf.parse("14/04/2023"), Especie.CANINA, t1 ) ;
 		tutorRepository.saveAll(Arrays.asList(t1));
+		pacienteRepository.saveAll(Arrays.asList(p1));
 	}
 
 }
